fac117409430feca569d864924e7c163c6551737
ostef <47391841+ostef@users.noreply.github.com>
2022-08-31 15:33:13 +0200

    Fixed invalid translation matrix and look at matrix generation, due to swapping of rows and columns.

--- source/mat4f/geom.c
+++ source/mat4f/geom.c
@@ -71,18 +71,18 @@ t_mat4f	ft_mat4f_translate(t_vec3f translation)
 	res.r0c0 = 1;
 	res.r0c1 = 0;
 	res.r0c2 = 0;
-	res.r0c3 = 0;
+	res.r0c3 = translation.x;
 	res.r1c0 = 0;
 	res.r1c1 = 1;
 	res.r1c2 = 0;
-	res.r1c3 = 0;
+	res.r1c3 = translation.y;
 	res.r2c0 = 0;
 	res.r2c1 = 0;
 	res.r2c2 = 1;
-	res.r2c3 = 0;
-	res.r3c0 = translation.x;
-	res.r3c1 = translation.y;
-	res.r3c2 = translation.z;
+	res.r2c3 = translation.z;
+	res.r3c0 = 0;
+	res.r3c1 = 0;
+	res.r3c2 = 0;
 	res.r3c3 = 1;
 	return (res);
 }
@@ -95,20 +95,20 @@ t_mat4f	ft_mat4f_look_at(t_vec3f origin, t_vec3f dir)
 
 	ft_vec3f_forward_to_orthonormal (dir, &right, &up);
 	res.r0c0 = right.x;
-	res.r0c1 = right.y;
-	res.r0c2 = right.z;
-	res.r0c3 = 0;
-	res.r1c0 = up.x;
+	res.r0c1 = up.x;
+	res.r0c2 = dir.x;
+	res.r0c3 = origin.x;
+	res.r1c0 = right.y;
 	res.r1c1 = up.y;
-	res.r1c2 = up.z;
-	res.r1c3 = 0;
-	res.r2c0 = dir.x;
-	res.r2c1 = dir.y;
+	res.r1c2 = dir.y;
+	res.r1c3 = origin.y;
+	res.r2c0 = right.z;
+	res.r2c1 = up.z;
 	res.r2c2 = dir.z;
-	res.r2c3 = 0;
-	res.r3c0 = origin.x;
-	res.r3c1 = origin.y;
-	res.r3c2 = origin.z;
+	res.r2c3 = origin.z;
+	res.r3c0 = 0;
+	res.r3c1 = 0;
+	res.r3c2 = 0;
 	res.r3c3 = 1;
 	return (res);
 }
